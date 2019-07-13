55cec91a2e28bff99755b8a55a11e80de43fc3fd
Taekyoon <tgchoi03@gmail.com>
2019-07-13 15:05:25 +0900

    added rotate string

---
+++ week_1/rotate_string.py
@@ -0,0 +1,27 @@
+def rotate_string(s1, s2):
+    """
+    >>> rotate_string('abcde', 'cdeab')
+    True
+    >>> rotate_string('abcde', 'cdeac')
+    False
+    """
+    if s1 == s2:
+        return True
+
+    s1 = s1 * 2
+    id2 = 0
+
+    for i in range(len(s1)):
+        if id2 >= len(s2):
+            return True
+        if s1[i] == s2[id2]:
+            id2 += 1
+        else:
+            id2 = 0
+
+    return False
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
