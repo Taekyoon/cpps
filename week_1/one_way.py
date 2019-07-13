d0d5452053f38c55185dca670e0f918fcee273b6
Taekyoon <tgchoi03@gmail.com>
2019-07-13 14:30:09 +0900

    added week_1

--- week_1/one_way.py
+++ week_1/one_way.py
@@ -3,13 +3,51 @@ def one_way(s1, s2):
     insert a character, remove a character, or replace a character. 
     Given two strings, write a function to check if they are one edit (or zero edits) away.
 
-    EXAMPLE
-    pale, ple -> true 
-    pales, pale -> true 
-    pale, bale -> true 
-    pale, bake -> false
+    >>> one_way('pale', 'ple')
+    True
+    >>> one_way('pales', 'pale')
+    True
+    >>> one_way('pale', 'bale')
+    True
+    >>> one_way('pale', 'bake')
+    False
     """
-    pass
+    if len(s1) == len(s2):
+        diff = 0
+        for c1, c2 in zip(s1, s2):
+            if diff > 1:
+                return False
+            if c1 != c2:
+                diff += 1
+        if diff > 1:
+            return False
+        return True
+    elif len(s1) != len(s2):
+        if abs(len(s1) - len(s2)) > 1:
+            return False
+
+        min_len = min([len(s1), len(s2)])
+
+        id1, id2 = 0, 0
+        act = 0
+        while id1 < min_len and id2 < min_len:
+            if act > 1:
+                return False
+            if s1[id1] == s2[id2]:
+                id1 += 1
+                id2 += 1
+            else:
+                if s1[id1 + 1] == s2[id2]:
+                    act += 1
+                    id1 += 1
+                else:
+                    act += 1
+                    id1 += 1
+                    id2 += 1
+        if act > 1:
+            return False
+        return True
+
 
 if __name__ == "__main__":
     import doctest
